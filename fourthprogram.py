94816ab14561a1004fd16ef8e62335f6b3aad585
Iqra Arif <bintearif79@gmail.com>
2025-04-23 04:56:12 -0700

    Add files via upload

---
+++ fourthprogram.py
@@ -0,0 +1,9 @@
+studentname="Iqra Arif"
+serialnumber="0023"
+address="Sargodha,Nehang,House no.36"
+phonenumber="03679427928"
+print(studentname)
+print(serialnumber)
+print(address)
+print(phonenumber)
+
